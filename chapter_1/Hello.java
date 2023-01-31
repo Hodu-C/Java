7357a9fd56ed237cb7819a74a323e15313eafb1a
Hodu-C <za4758@icloud.com>
2023-01-31 16:03:53 +0900

    create repo

---
+++ chapter_1/Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import java.util.*;
+public class Hello{
+    public static void main(String[] args) {
+        long octNum = -14L;
+        float test = 3.12345678901234567890f;
+        float test2 = 123.12341978901234567890f;
+        float test3 = 3.12345678901234567890f;
+        char hch = '핳';
+        char jch = '가';
+        int sum_ = hch-jch;
+        int i = 300;
+        double f = 1;
+        float f2 = 1234;
+        String sTest = "Customerer";
+        System.out.printf("%28.26f%n%28.26f%n%28.26f%n", test,test2,test3);
+        System.out.printf("%s%n", Integer.toBinaryString(i));
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
