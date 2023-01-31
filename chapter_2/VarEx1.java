5442a349a800320de2f9f163772d154935da908f
Hodu-C <za4758@icloud.com>
2023-02-01 08:43:19 +0900

    변수 선언 및 초기화 연습

---
+++ chapter_2/VarEx1.java
@@ -0,0 +1,10 @@
+package chapter_2;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int year = 0; //선언과 초기화
+        int age = 14;
+
+        System.out.println(year + age);
+    }
+}
